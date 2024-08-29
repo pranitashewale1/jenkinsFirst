4951882f81beba607ba0b2de5c6236eeeae3d981
Manish Kumar Saraf <mksaraf@outlook.com>
2024-08-28 22:25:40 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,8 +1,12 @@
 # hello.py
-def my_function():
-    print("Hello, world! Testing")
+def my_function1():
+    print("Hello, world! Testing 111")
     # Your function code here
     pass
-
+def my_function2():
+    print("Hello, world! Testing222")
+    # Your function code here
+    pass
 if __name__ == "__main__":
-    my_function()
+    my_function1()
+    my_function2()
