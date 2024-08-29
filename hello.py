acc56892010d9d042f2c2587b3214ace0908fe38
Manish Kumar Saraf <mksaraf@outlook.com>
2024-08-28 22:22:03 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,10 @@
-print('Hello World')
+# hello.py
+def myfunction1():
+    # put your function code here
+    print("Hello from myfunction1!")
+def myfunction2():
+    # put your function code here
+    print("Hello from myfunction2!")
+
+myfunction1()
+myfunction2()
