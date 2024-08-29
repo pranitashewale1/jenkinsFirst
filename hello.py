e45bf5c7155d343576fbc0d434f47b38329e43b9
Manish Kumar Saraf <mksaraf@outlook.com>
2024-08-28 22:33:06 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,10 +3,18 @@ def my_function1():
     print("Hello, world! Testing 111")
     # Your function code here
     pass
+
 def my_function2():
-    print("Hello, world! Testing222")
+    print("Hello, world! Testing 222")
     # Your function code here
     pass
+
 if __name__ == "__main__":
-    my_function1()
-    my_function2()
+    import sys
+    if len(sys.argv) > 1:
+        func_name = sys.argv[1]
+        globals()[func_name]()
+    else:
+        my_function1()
+        my_function2()
+
